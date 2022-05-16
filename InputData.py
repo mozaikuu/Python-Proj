8679209fde8d763280b651b3874cc71b4cf2f29a
Mozaiku <80972213+mozaikuu@users.noreply.github.com>
2022-05-16 10:07:31 +0200

    commit]

--- InputData.py
+++ InputData.py
@@ -1,11 +1,12 @@
-name = input ("What is your name? ")
-age = input ("What is your age? ")
-if 18 <= int(age) :
+try:
+ Name = input ("What is your name? ")
+ age = input ("What is your age? ")
+ if 18 <= int(age) :
   print ("Fuck you, " + name + "!" )
-elif int(age) > 18 : 
+ elif int(age) > 18 : 
   print ("You are not old enough to fuck around, " + name + "!")
-else :
-   print("do you think im fucking dumb shithead? i may not be able to read but i know my numbers well and those arent fucking numbers. u must have failed math school to make that mistake. bro are you 5?")
+except :
+  print("do you think im fucking dumb shithead? i may not be able to read but i know my numbers well and those arent fucking numbers. Go to math school, learn some numbers then come back and try again.")
 #   print("i made this myself!") 
 #   print("👌")  these dont work sadly ahmed 
 
