d41cde73fc808b49545349cdf402ae1bd7efd85c
Mozaiku <80972213+mozaikuu@users.noreply.github.com>
2022-05-16 08:24:40 +0200

    MOORE

---
+++ functions.py
@@ -0,0 +1,15 @@
+#function = organize code
+#def = define to call function
+def say_hi(name):
+    print("sup  " + name)
+
+say_hi(daveeeeeeyyyyyy)
+
+
+def cube(num):
+    return num*num*num
+
+print(cube(10))
+
+
+   
---
+++ tuples.py
@@ -0,0 +1,8 @@
+#tuples are like lists data structures that are immutable 
+coords = [(4, 5)] #list of tuples   list=[]  tuple=()
+print(coords[0]) # prints the first element of the tuple
+print(coords[1]) # prints the second element of the tuple
+print(coords[-1]) # prints the last element of the tuple    
+print(coords[-2]) # prints the second to last element of the tuple  
+
+
